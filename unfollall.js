3f379cec569ae1ef6e7d40e46083fa55d1acf041
I Gusti Bagus Sudarsana <gung.ana0@gmail.com>
2019-01-12 10:33:38 +0700

    Update unfollall.js

--- unfollall.js
+++ unfollall.js
@@ -111,8 +111,8 @@ console.log(chalk`
 
   ——————————————————  [THANKS TO]  ————————————————————
   [✓] CODE BY CYBER SCREAMER CCOCOT (ccocot@bc0de.net)
-  [✓] FIXING & TESTING BY SYNTAX (@officialputu_id)
-  [✓] CCOCOT.CO | BC0DE.NET | NAONLAH.NET | WingkoColi
+  [✓] FIXING & TESTING BY I Gusti Bagus Sudarsana, S.E
+  [✓] Follow account Instagram @gush_na dan @idberbagi
   [✓] SGB TEAM REBORN | Zerobyte.id | ccocot@bc0de.net 
   —————————————————————————————————————————————————————
   What's new?
